aeeab0eea6fa9bc4499c255f53626a1c753ddcd5
daniel <tishakovdanyilgmail.com>
2024-01-29 14:14:41 +0000

    init commit

---
+++ main.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+from collections import UserList, deque
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+class NoRight(Exception):
+    pass
+
+class Disease:
+    def __init__(self, name, doctor):
+        self.name = name
+        self.doctor = doctor
+        self.treatment = list()
+        self.time = datetime.now()
+        self.cured = False
+        self.cure_time = None
+
+    def cure(self):
+        self.cured = True
+        self.cure_time = datetime.now()
+
+    def treat(self, treatment):
+        self.treatment.append(treatment)
+
+class Story(UserList):
+    story_size = 10
+
+    def __init__(self):
+        self.__data = deque([], self.story_size)
+        self.__data__dict = dict()
+        self.data = list()
+    
+    def __getitem__(self, key=None):
+        if key is None:
+            return list(self.__data)
+        if isinstance(key, int):
+            return self.__data[key]
+        elif isinstance(key, str):
+            return self.__data__dict.get(key)
+    
+    def __len__(self):
+        return len(self.__data)
+    
+    def add(self, *_):
+        raise NoRight
+    
+    def __add(self, disease):
+        self.__data.append(disease)
+        self.__data__dict = {des.name: des for des in self.__data}
+        self.data = [des for des in self.__data]
+    
+    @property
+    def healthy(self):
+        return all([disease.cured for disease in self.__data])
+
+class Patient(Person):
+    def __init__(self, name, surname, polis):
+        super().__init__(name, surname)
+        self.polis = polis
+        self.story = Story()
+
+    @property
+    def healthy(self):
+        return self.story.healthy
+
+class Doctor(Person):
+    def __init__(self, name, surname, position):
+        super().__init__(name, surname)
+        self.position = position
+        
+    def add(self, patient, disease_name):
+        des = Disease(disease_name, " ".join((self.name, self.surname)))
+        patient.story._Story__add(des)
+    
+    def treat(self, patient, disease_name, treatment):
+        patient.story[disease_name].treat(treatment)
+
+
+doctor_surg = Doctor("Bill", "Klinton", "Head_doctor")
+doctor_tera = Doctor("Tom", "Jerry", "Therapist")
+
+patient_one = Patient("George", "Bush", 1234567890)
+patient_two = Patient("Barak", "Obama", 976543210)
+
+doctor_surg.add(patient_one, "flu")
+doctor_tera.add(patient_two, "appendicite")
+
+print(patient_one.story["flu"].time)
+print(patient_two.story["appendicite"].time)
+
+doctor_surg.treat(patient_one, 'flu', 'pills')
+doctor_tera.treat(patient_two, "appendicite", "operations")
+
+print(patient_one.story['flu'].treatment)
+print(patient_two.story["appendicite"].treatment)
+
+print(patient_one.healthy)
+
+patient_one.story['flu'].cure()
+print(patient_one.healthy)
